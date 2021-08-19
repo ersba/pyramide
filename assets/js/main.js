9357cdaf79084627100b72527e984c9cc6a83b96
erik-ramich <erik.ramich@gmx.de>
2021-08-19 23:53:03 +0200

    draw funktioniert und karten sind größer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 var suits = ["spades", "diamonds", "clubs", "hearts"];
-var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+var values = ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"];
 // var deck = [{Value: 'A', Suit: 'spades'}, {Value: 'A', Suit: 'diamonds'}, {Value: 'A', Suit: 'clubs'}{Value: 'A', Suit: 'hearts'} //Das ganze Karten Deck deklariert
 //             {Value: 'K', Suit: 'spades'}, {Value: 'K', Suit: 'diamonds'}, {Value: 'K', Suit: 'clubs'}{Value: 'K', Suit: 'hearts'}
 //             {Value: 'Q', Suit: 'spades'}, {Value: 'Q', Suit: 'diamonds'}, {Value: 'Q', Suit: 'clubs'}{Value: 'Q', Suit: 'hearts'}
@@ -12,22 +12,32 @@ var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 //             {Value: '4', Suit: 'spades'}, {Value: '4', Suit: 'diamonds'}, {Value: '4', Suit: 'clubs'}{Value: '4', Suit: 'hearts'}
 //             {Value: '3', Suit: 'spades'}, {Value: '3', Suit: 'diamonds'}, {Value: '3', Suit: 'clubs'}{Value: '3', Suit: 'hearts'}
 //             {Value: '2', Suit: 'spades'}, {Value: '2', Suit: 'diamonds'}, {Value: '2', Suit: 'clubs'}{Value: '2', Suit: 'hearts'}]
-
+var deck = new Array();
 function getDeck() //Hier werden die 2 arrays zusammen geführt
 {
-	var deck = new Array();
+	
 
 	for(var i = 0; i < suits.length; i++)
 	{
 		for(var x = 0; x < values.length; x++)
 		{
 			var card = {Value: values[x], Suit: suits[i]};
+			console.log("assets/img/cards/"+values[x]+"_"+suits[i]+"_white.png");
+	// 		for (var i = 0; ;)//vielleicht wird das noch
+    // {
+    //     document.getElementById("grid-item"+i+"_card").src="../img/cards/"+values[x]+"_"+suits[i]+"_white.png";//Hier junge
+    // 	console.log("assets/img/cards/"+values[x]+"_"+suits[i]+"_white.png")	}	
 			deck.push(card);
+			console.log(deck);
+		
 		}
 	}
 
 	return deck;
 }
+getDeck();
+
+
 function shuffle(deck){
     // for 1000 turns
     // switch the values of two random cards
@@ -41,6 +51,13 @@ function shuffle(deck){
         deck[location2] = tmp;
     }
 }
+shuffle(deck);//hier wird gemischt
+
+    for (var i = 1; i < 11; i++)//draw funktioniert!
+    {
+        document.getElementById("grid-item"+i+"_card").src="assets/img/cards/"+deck[i].Value+"_"+deck[i].Suit+"_white.png";//Hier junge
+    }
+
 function renderDeck(deck)
 {
       document.getElementById("deck").innerHTML = "";
@@ -61,5 +78,4 @@ function renderDeck(deck)
 		document.getElementById("deck").appendChild(card);
 	}
 }
-
 
